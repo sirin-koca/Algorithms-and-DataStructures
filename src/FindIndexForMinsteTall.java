fd61edd8ca524f113e2196efdac2e07bc5073c37
Sirin Koca <s182211@oslomet.no>
2022-09-05 16:14:26 +0200

    commit

--- src/FindIndexForMinsteTall.java
+++ src/FindIndexForMinsteTall.java
@@ -1,8 +1,16 @@
 public class FindIndexForMinsteTall {
 
-    int[] a={8, 4, 17, 10, 6, 20, 1, 11, 15, 3, 18, 9, 2, 7, 19};
+    public static void main(String[] args) {
+        int[] a = {8, 4, 17, 10, 6, 20, 1, 0, 11, 15, 3, 18, 9, 2, -7, 19};
 
-    public int min(int[] a){
+        int minIndex = min(a);
+
+        System.out.println("Index number of the smallest number in this array is: " + minIndex);
+    }
+
+
+
+    static int min(int[] a){
         int minValue = a[0];
         int minIndex = 0;
 
