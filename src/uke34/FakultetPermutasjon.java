d296389e9564f292dc7ce0af959c32a284ac9eec
Sirin Koca <s182211@oslomet.no>
2022-09-06 22:28:05 +0200

    initial commit

--- src/uke34/FakultetPermutasjon.java
+++ src/uke34/FakultetPermutasjon.java
@@ -5,16 +5,17 @@ public class FakultetPermutasjon {
     public static void main(String[] args) {
 
         System.out.println("n! Fakultet betyr n*(n-1)*...*1. La os teste n=9 => 9! er: " + fak(9));
-     }
+    }
 
-    public static long fak(int n){
+    public static long fak(int n) {
         if (n < 0)
             throw new IllegalArgumentException("n < 0");
 
         long fak = 1;
 
-        for (int i = 2; i <= n; i++) fak *= i;
-
+        for (int i = 2; i <= n; i++) {
+            fak *= i;
+        }
         return fak;
     }
 
