1ea4df31ceb48049f19fc6be2d1c6891236e911f
Sirin Koca <s182211@oslomet.no>
2022-09-12 01:42:55 +0200

    Finne den beste måte til å generere en random tabell (array)

---
+++ src/uke35/RandomTabellSisteVersjon.java
@@ -0,0 +1,33 @@
+package uke35;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class RandomTabellSisteVersjon {
+
+    // int[] a = {4, 2, 6, 1, 78, 32, 22, 90, 9, 10};
+
+       public static void main(String[] args) {
+        System.out.println(randPerm(5));
+    }
+    public static void bytt(int[] a, int i, int j)
+    {
+        int temp = a[i]; a[i] = a[j]; a[j] = temp;
+    }
+
+    public static int[] randPerm(int n)  // en effektiv versjon
+    {
+        Random r = new Random();         // en randomgenerator
+        int[] a = new int[n];            // en tabell med plass til n tall
+
+        Arrays.setAll(a, i -> i + 1);    // legger inn tallene 1, 2, . , n
+
+        for (int k = n - 1; k > 0; k--)  // løkke som går n - 1 ganger
+        {
+            int i = r.nextInt(k+1);        // en tilfeldig tall fra 0 til k
+            bytt(a,k,i);                   // bytter om
+        }
+
+        return a;                        // permutasjonen returneres
+    }
+}
