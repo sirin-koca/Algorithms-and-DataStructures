11d992a7c1f8c82c14812963be1cd7064ec5d943
Sirin Koca <s182211@oslomet.no>
2022-09-12 02:10:38 +0200

    java.util IntStream klasse

---
+++ src/uke36/IntStream.java
@@ -0,0 +1,20 @@
+package uke36;
+
+import java.util.Arrays;
+import java.util.OptionalInt;
+
+public class IntStream {
+    public class Program {
+        public static void main(String ... args)
+        {
+            int[] a = {8,3,5,7,9,6,10,2,1,4};  // en heltallstabell
+            java.util.stream.IntStream s = Arrays.stream(a);    // fra int-tabell til IntStream
+
+            OptionalInt resultat = s.max();    // kaller max-metoden
+
+            if (resultat.isPresent()) System.out.println(resultat.getAsInt());
+            else System.out.println("Ingen verdi!");
+        }
+    }
+
+}
