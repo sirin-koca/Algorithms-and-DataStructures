75d283f9da823f9ff50eb9a1993893ebd93e2be5
Sirin Koca <s182211@oslomet.no>
2022-09-12 13:23:14 +0200

    finne antall inversjon i en tabell / array

---
+++ src/uke36/Inversjon.java
@@ -0,0 +1,18 @@
+package uke36;
+
+public class Inversjon {
+    public static void main(String[] args) {
+        int[] a = {1, 2, 6, 3, 0};
+        System.out.println(inversjoner(a));
+    }
+    public static int inversjoner(int[] a)
+    {
+        int antall = 0;  // antall inversjoner
+        for (int i = 0; i < a.length - 1; i++) {
+            for (int j = i + 1; j < a.length; j++) {
+                if (a[i] > a[j]) antall++;  // en inversjon siden i < j
+            }
+        }
+        return antall;
+    }
+}
