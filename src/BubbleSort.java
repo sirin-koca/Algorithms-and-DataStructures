cb6b3611437b4ccb795ed5a899e1411226d7e626
Sirin Koca <s182211@oslomet.no>
2022-10-01 23:48:39 +0200

    Bubble Sort Algorithm

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -1,6 +1,6 @@
-public class Bubble {
+public class BubbleSort {
     public static void main(String[] args) {
-        int[] a = {1, 9, 2, 4, 7, 6};
+        int[] a = {9, 8, 7, 6, 5, 4, 3, 2, 1};
 
         bubble(a);
 
@@ -9,27 +9,33 @@ public class Bubble {
         // for(int k : a){ System.out.println(k);}
     }
 
+    // Denen metoden flytter største tallet 9 helt bakerts i listen.
+    // For å visualisere, printer ut hver iterasjon
     public static void bubble(int[] a) {
         int begin = 0;
-        int end = a.length - 1;
+        int end = a.length;
 
-        for (int i = begin; i < end; i++) {
+        for (int i = 0; i < end-1; i++) {
+            System.out.print(a[i] + ", ");
+        }
+        System.out.print(1); // for å få 1 på slutten uten komma.
+
+        System.out.println();
+
+        for (int i = begin; i < end - 1; i++) {
             if (a[i] > a[i + 1]) {
-
                 // BYTT!
                 int temp = a[i]; // vi må bruke en hjelpevariabel "temp"
                 a[i] = a[i + 1];
                 a[i + 1] = temp;
-            } else {
-                // Gjør ingenting - fordi da er ingen inverjson
             }
 
             System.out.print(a[0]);
-            for (int j = 0; j < a.length; j++) {
+            for (int j = 1; j < end; j++) {
                 System.out.print(", " + a[j]);
             }
             System.out.println();
+
         }
-        a[0] = 99;
     }
 }
