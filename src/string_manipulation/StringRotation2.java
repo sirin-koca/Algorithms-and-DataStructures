06888cca42bdb62ce200357df1007f6687fea14d
Sirin Koca <s182211@oslomet.no>
2023-10-02 14:58:50 +0200

    Palindrome and AnagramChecker - string manipulation programs

--- src/string_manipulation/StringRotation2.java
+++ src/string_manipulation/StringRotation2.java
@@ -34,5 +34,27 @@ public class StringRotation2 {
         System.out.println(isRotation("hello", "ohell"));  // Should print true
         System.out.println(isRotation("hello", "ohelle"));  // Should print false
     }
-
 }
+
+
+/*
+If a string b is a rotation of string "a" then it can be broken into two parts: x and y, such that "a" can be expressed
+as xy and b can be expressed as yx.
+
+For instance, with a = "waterbottle" and b = "erbottlewat", we have:
+x = "wat" and y = "erbottle". Thus, a = xy = "waterbottle" and b = yx = "erbottlewat".
+
+If we concatenate a with itself, the result is a + a = xyxy.
+
+Continuing with the example: a + a = "waterbottlewaterbottle"
+
+If b is indeed a rotation of "a", then b (or yx) will always be a substring of a + a (or xyxy).
+
+In our example, "erbottlewat" is a substring of "waterbottlewaterbottle".
+
+By concatenating the string with itself, we effectively create all possible rotations of the original string,
+lined up one after another. So, if b is any rotation of "a", it will appear in a + a.
+
+This technique allows us to solve the rotation problem by transforming it into a substring problem.
+
+ */
